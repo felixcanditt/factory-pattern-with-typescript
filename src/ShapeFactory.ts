746dab2832cd20aaa3ba8c37507c2e28dda5a56e
Felix Canditt <felix.canditt@gmail.com>
2025-07-17 16:04:06 +0200

    added function createShape, added interfaces

--- src/ShapeFactory.ts
+++ src/ShapeFactory.ts
@@ -1,7 +1,39 @@
 import { Circle } from './Circle';
 import { Rectangle } from './Rectangle';
 
+interface CircleInterface {
+  typeString: 'circle';
+  x: number;
+  y: number;
+  r: number;
+}
+
+interface RectangleInterface {
+  typeString: 'rectangle';
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
 export class ShapeFactory {
+  static createShape(
+    shapeSpecs: CircleInterface | RectangleInterface
+  ): Circle | Rectangle | string {
+    if (shapeSpecs.typeString === 'circle') {
+      return new Circle(shapeSpecs.x, shapeSpecs.y, shapeSpecs.r);
+    } else if (shapeSpecs.typeString === 'rectangle') {
+      return new Rectangle(
+        shapeSpecs.x,
+        shapeSpecs.y,
+        shapeSpecs.w,
+        shapeSpecs.h
+      );
+    } else {
+      return "typeString needs to be 'circle' or 'rectangle'";
+    }
+  }
+
   static createCircle(typeString: string, x: number, y: number, r: number) {
     return new Circle(x, y, r);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,42 @@
 import { ShapeFactory } from './ShapeFactory';
 
 function main() {
-  const newCircle = ShapeFactory.createCircle('circle', 1, 2, 3);
-  console.log(
-    newCircle.toString(),
-    newCircle.origin(),
-    'Area:',
-    newCircle.returnArea(),
-    'Circumference:',
-    newCircle.returnCircumference()
-  );
+  const newCircle = ShapeFactory.createShape({
+    typeString: 'circle',
+    x: 1,
+    y: 2,
+    r: 3,
+  });
+  if (typeof newCircle != 'string') {
+    console.log(
+      newCircle.toString(),
+      newCircle.origin(),
+      'Area:',
+      newCircle.returnArea(),
+      'Circumference:',
+      newCircle.returnCircumference()
+    );
+  } else {
+    console.log(newCircle);
+  }
 
-  const newRectangle = ShapeFactory.createRectangle('rectangle', 1, 2, 3, 4);
-  console.log(
-    newRectangle.toString(),
-    newRectangle.origin(),
-    'Area:',
-    newRectangle.returnArea(),
-    'Circumference:',
-    newRectangle.returnCircumference()
-  );
+  const newRectangle = ShapeFactory.createShape({
+    typeString: 'rectangle',
+    x: 1,
+    y: 2,
+    w: 3,
+    h: 4,
+  });
+  if (typeof newRectangle != 'string') {
+    console.log(
+      newRectangle.toString(),
+      newRectangle.origin(),
+      'Area:',
+      newRectangle.returnArea(),
+      'Circumference:',
+      newRectangle.returnCircumference()
+    );
+  }
 }
 
 main();
