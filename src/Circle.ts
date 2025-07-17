1e335dd36dda970783726177de579e78b3e001b0
Felix Canditt <felix.canditt@gmail.com>
2025-07-17 10:14:05 +0200

    updated Circle.ts

--- src/Circle.ts
+++ src/Circle.ts
@@ -2,17 +2,25 @@ import { Shape } from './Shape';
 
 class Circle extends Shape {
   private radius: number;
+  private area: number;
+  private circumference: number;
 
   constructor(xOrigin: number, yOrigin: number, radius: number) {
     super(xOrigin, yOrigin);
     this.radius = radius;
+    this.area = this.radius * this.radius * 3.14159;
+    this.circumference = 2 * this.radius * 3.14159;
   }
 
-  calculateArea(): number {
-    return this.radius * this.radius * 3.14159;
+  returnArea(): number {
+    return this.area;
   }
 
-  calculateCircumference(): number {
-    return 2 * this.radius * 3.14159;
+  returnCircumference(): number {
+    return this.circumference;
+  }
+
+  toString(): string {
+    return `The circle has an area of ${this.area} and a circumference of ${this.circumference}.`;
   }
 }
