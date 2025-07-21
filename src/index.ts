ce6fc82561f69eb7780415e4c3dfd65276d626d3
Felix Canditt <felix.canditt@gmail.com>
2025-07-21 11:40:07 +0200

    refactored console log

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,41 @@
 import { ShapeFactory } from './ShapeFactory';
+import { Circle } from './Circle';
+import { Rectangle } from './Rectangle';
+
+main();
 
 function main() {
-  const newCircle = ShapeFactory.createShape({
+  const firstShape = ShapeFactory.createShape({
     typeString: 'circle',
     xOrigin: 1,
     yOrigin: 2,
     radius: 3,
   });
-  if (typeof newCircle != 'string') {
-    console.log(
-      newCircle.toString(),
-      newCircle.origin(),
-      'Area:',
-      newCircle.returnArea(),
-      'Circumference:',
-      newCircle.returnCircumference()
-    );
-  } else {
-    console.log(newCircle);
-  }
 
-  const newRectangle = ShapeFactory.createShape({
+  const secondShape = ShapeFactory.createShape({
     typeString: 'rectangle',
     xOrigin: 1,
     yOrigin: 2,
     width: 3,
     height: 4,
   });
-  if (typeof newRectangle != 'string') {
-    console.log(
-      newRectangle.toString(),
-      newRectangle.origin(),
-      'Area:',
-      newRectangle.returnArea(),
-      'Circumference:',
-      newRectangle.returnCircumference()
-    );
-  }
+
+  showResult(firstShape);
+  showResult(secondShape);
 }
 
-main();
+function showResult(factoryResult: Circle | Rectangle | string) {
+  if (typeof factoryResult != 'string') {
+    // ShapeFactory returns a string if typeString isn't provided correctly
+    console.log(
+      factoryResult.toString(),
+      factoryResult.origin(),
+      'Area:',
+      factoryResult.returnArea(),
+      'Circumference:',
+      factoryResult.returnCircumference()
+    );
+  } else {
+    console.log(factoryResult);
+  }
+}
